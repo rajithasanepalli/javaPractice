9c90c0adffaae6b046ebfa896512f5a9ded1783c
Rajitha SANEPALLI <rajithas.in@mouritech.com>
2022-01-04 23:00:27 +0530

    7 java files committed

---
+++ SubArray.java
@@ -0,0 +1,20 @@
+package javaBasics;
+
+import java.util.Arrays;
+
+public class SubArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+	
+		for(int i=0;i<=10;i++) {
+			System.out.print(""+a[i]+",");
+		}
+		System.out.println("\n");
+		int []b=Arrays.copyOfRange(a, 3, 8);
+		System.out.println(Arrays.toString(b));
+
+	}
+
+}
