0d12975db94d04eb3d6b0b81880d968d9cd6ac4b
Rajitha SANEPALLI <rajithas.in@mouritech.com>
2022-01-05 17:21:11 +0530

    oops files committed

---
+++ AuthorManagement.java
@@ -0,0 +1,39 @@
+package oopsconcept;
+
+import java.util.Scanner;
+
+public class AuthorManagement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// create a book object
+		Author b = new Author();
+		char ch = 'y';// local variable because it is defined inside main method
+
+		while (ch == 'y') {
+			System.out.println("Enter the Transaction.....");
+			System.out.println("Enter 'read' to enter author details.....");
+			System.out.println("Enter 'display' to view author details.....");
+			String transaction = sc.next();
+
+			switch (transaction) {
+			case "read":
+				b.readAuthorDetails();
+				break;
+			case "display":
+				b.displyAuthorDetails();
+				break;
+			default:
+				System.out.println("Wrong choice....");
+				break;
+			}// close of switch
+			System.out.println("Enter the choice to continue.....");
+			ch = sc.next().charAt(0);
+		} // close of while
+
+
+
+	}
+
+}
---
+++ RemoveDupFromString.java
@@ -0,0 +1,16 @@
+package javaBasics;
+
+public class RemoveDupFromString {
+
+	public static void main(String[] args) {
+		String s1="Rajitha";
+		String s2=new String();
+		System.out.println("Intial string="+s1);
+		//Removing duplicates from string
+		
+		
+		
+
+	}
+
+}
